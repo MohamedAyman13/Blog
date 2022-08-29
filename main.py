a7e8a4e7730a2f3f11abb3dc97d7f09f864bfa2f
mohamed <mohamedaayn@gmail.com>
2022-08-29 02:42:53 -0700

    enw commit

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ from flask import abort
 import os
 import smtplib
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = "hfjksdlfhnjdsfvds4fvds46f4ds9f6sd4g98f5sd4gdsfvsdf6g4sa68f4sd68f7"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
